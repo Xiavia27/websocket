e7e0c1a451e2163c78dc589c7e6c7080e60e3de3
Ant <tony1latorre@hotmail.com>
2020-05-04 06:31:10 -0500

    Adding websocket base

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var socket = require('socket.io');
+
+
+//App Setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log("Listening to requests on port 4000")
+});
+
+//Static files
+app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('Connection made with socket ', socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+});
---
+++ public/websocket.js
@@ -0,0 +1,37 @@
+//Make connection
+var socket = io.connect('http://localhost:4000');
+
+//Dom
+var message = document.getElementById('message'),
+    handle = document.getElementById('handle'),
+    button = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+
+//Emitters
+
+button.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value, 
+        handle: handle.value
+    });
+});
+
+
+//Listeners
+
+socket.on('chat', function(data){
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+    feedback.innerHTML = '';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...<em><p>';
+});
+
+//Broadcasters
+
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+});
